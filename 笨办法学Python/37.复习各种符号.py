74355ed1fed25f3f9597633332e97df19819038a
康兵奎 <178974662@qq.com>
2018-05-07 18:48:19 +0800

    keep leaning

---
+++ 笨办法学Python/37.复习各种符号.py
@@ -0,0 +1,59 @@
+# 复习 python 关键字和各种符号
+
+# 关键字
+
+# and
+# del
+# form
+# not
+# while
+# as
+# elif
+# global
+# or
+# with
+# assert
+# else
+# if
+# pass
+# yield
+# break
+# except
+# import
+# print
+# class
+# exec
+# in
+# raise
+# continue
+# finally
+# is
+# return
+# def
+# for
+# lambda
+# try
+
+# 数据类型
+
+# True
+# False
+# None
+# strings
+# numbers
+# floats
+# lists
+
+# 字符串转移序列
+
+# \\   ===> \
+# \'   ===> '
+# \"   ===> "
+# \a   ===> 响铃
+# \b   ===> 退格
+# \f   ===> 换页
+# \n   ===> 换行
+# \r   ===> 回车
+# \t   ===> 水平制表符
+# \v   ===> 垂直制表符
+
