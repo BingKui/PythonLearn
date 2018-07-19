c849a810e8a9fbfe9e8ee89701f741470d22def0
康兵奎 <178974662@qq.com>
2018-07-19 15:16:13 +0800

    图片合成

--- 图片合成/musicPicture.py
+++ 图片合成/musicPicture.py
@@ -1,7 +1,100 @@
 # 引入需要的库
 from PIL import Image, ImageDraw, ImageFont
-def add_text(img, name, position):
+from io import BytesIO
+import pathlib
+import requests
+import qrcode
+
+def add_text(img, text, size, position, color="#9400D3"):
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('MFJingYue.ttf', 50)
-    color = "#333333"
-    draw.text(position, unicode(name, 'UTF-8'), font, fill=color)
+    font = ImageFont.truetype('MFJingYue.ttf', size)
+    draw.text(position, text, fill=color, font=font, align="center")
+
+# 获取每行第一个字的横坐标
+# 根据字体大小处理
+# 每行最多14个字
+def get_x_position(len):
+    max = 14
+    x = 50 + (max - len) * 25 / 2
+    return x
+# 生成二维码
+def make_qrcode(url, type):
+    qr = qrcode.QRCode(
+        version=5,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=8,
+        border=3
+    )
+    qr.add_data(url)
+    # qr.make(fit = True)
+    img = qr.make_image()
+    img = img.convert("RGBA")
+    icon = None
+    if type == 'netease':
+        icon = Image.open(pathlib.Path('netease.png'))
+    elif type == 'qq':
+        icon = Image.open(pathlib.Path('qq.png'))
+    img_w, img_h = img.size
+    factor = 4
+    size_w = int(img_w/factor)
+    size_h = int(img_h/factor)
+    
+    icon_w, icon_h = icon.size
+    if icon_w > size_w:
+        icon_w = size_w
+    if icon_h > size_h:
+        icon_h = size_h
+    icon = icon.resize((icon_w, icon_h), Image.ANTIALIAS)
+    
+    w = int((img_w-icon_w)/2)
+    h = int((img_h-icon_h)/2)
+    img.paste(icon, (w,h), icon)
+    return img
+
+def merge_img(img_src, name, count, address, author=""):
+    name = unicode(name, 'UTF-8')
+    count = unicode("播放量：" + count, 'UTF-8')
+    author = unicode("创建人：" + author, 'UTF-8')
+    background_img = Image.open(pathlib.Path('background.jpg'))
+    # 获取远程图片
+    response = requests.get(img_src)
+    img = Image.open(BytesIO(response.content))
+    # 设置图片替换位置
+    box = (100, 50, 350, 300)
+    # 选取图片
+    region = img
+    region = region.resize((box[2] - box[0], box[3] - box[1]))
+    # 粘贴到背景图上
+    background_img.paste(region, box)
+    # 添加名字
+    name_len = len(name)
+    if name_len <= 14:
+        x = get_x_position(name_len)
+        add_text(background_img, name, 25, (x, 330))
+    else:
+        name_one = name[0:14]
+        print name_one
+        add_text(background_img, name_one, 25, (50, 330))
+        name_two = name[14:name_len]
+        print name_two
+        x = get_x_position(len(name_two))
+        add_text(background_img, name_two, 25, (x, 370))
+    # 添加创建者
+    add_text(background_img, author, 20, (50, 560), color='white')
+    # 添加播放数
+    add_text(background_img, count, 20, (50, 600), color='white')
+    # 生成二维码地址
+    qrcode_img = make_qrcode(address, 'qq')
+    # 粘贴到背景图上
+    box_qrcode = (300, 527, 425, 652)
+    print qrcode_img.size, qrcode_img.mode
+    # qrcode_img.save('./qrcode.png')
+    region_qrcode = qrcode_img
+    region_qrcode = region_qrcode.resize((box_qrcode[2] - box_qrcode[0], box_qrcode[3] - box_qrcode[1]))
+    background_img.paste(region_qrcode, box_qrcode)
+    # 保存图片
+    background_img.save('./out.jpg')
+
+url = 'https://p.qpic.cn/music_cover/xCubmCUxl5icKI16GSM3EXibUJ7Fq7ibq3pIO2ljRa94V56BflX5Yze4w/300?n=1'
+address = 'https://y.qq.com/n/yqq/playsquare/2345289098.html#stat=y_new.playlist.pic_click'
+merge_img(url, '全是回忆！那些年火到不行的校园情歌', '1156.4万', address, author="安多")
--- 图片合成/picture.py
+++ 图片合成/picture.py
@@ -7,7 +7,7 @@ import pathlib
 import urllib2
 import requests
 
-base_img = Image.open(pathlib.Path('1.png'))
+base_img = Image.open(pathlib.Path('brianda.jpg'))
 url = 'https://p.qpic.cn/music_cover/gaSSCRswoq7NlpHA8vK1PqGr6rCVbibywq9UsT7ZEeT1Ur5oFmibzWhA/300?n=1'
 # req = urllib2.Request(url)
 # html = urllib2.urlopen(req, timeout=10)
@@ -17,22 +17,22 @@ link_img = Image.open(BytesIO(response.content))
 # 可以查看图片的 size 和 mode
 print link_img.size, link_img.mode
 # 底图上需要 p 掉的区域
-box = (166, 64, 320, 337)
+box = (100, 50, 350, 300)
 
 # 加载需要合成的图片
 tmp_img = Image.open(pathlib.Path('2.png'))
 # 这里选择一块区域或者整张图片
 # 选取整整图片
-region = tmp_img
+region = link_img
 
 region = region.resize((box[2] - box[0], box[3] - box[1]))
 base_img.paste(region, box)
 
 # 使用自定义字体
-font = ImageFont.truetype('MFJingYue.ttf', 50);
+font = ImageFont.truetype('font.otf', 50);
 
 draw = ImageDraw.Draw(base_img)
 # 防止乱码，做 Unicode 处理
-draw.text((0, 0), unicode('康兵奎!','UTF-8'), '#333333', font)
+draw.text((0, 0), unicode('这个是一句话!','UTF-8'), '#333333', font)
 
 base_img.save('./out.png')
